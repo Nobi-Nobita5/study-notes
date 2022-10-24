e247ae00c290cffc42535700b325a4b77248c930
xionghx <419330259@qq.com>
2022-10-24 22:51:28 +0800

    提交Hadoop范例代码

---
+++ 1.大数据学习笔记/13.书籍&阅读笔记&&案例代码/Hadoop权威指南/Hadoop/src/main/java/Chapter2/E_2_3_MaxTemperatureMapper.java
@@ -0,0 +1,13 @@
+package Chapter2;
+
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+
+/**
+ * @Author: Xionghx
+ * @Date: 2022/10/24/22:09
+ * @Version: 1.0
+ */
+public class E_2_3_MaxTemperatureMapper extends MapReduce{
+}
