f8f5165ec5db5b200cebea9c30ad535ed79f25d7
xionghx <419330259@qq.com>
2023-03-16 22:08:14 +0800

    Java SE笔记提交

--- 6.编程语言/1.Java/0.代码/Test.java
+++ 6.编程语言/1.Java/0.代码/Test.java
@@ -1,30 +1,32 @@
+import JavaSE.设计模式.Single.Singleton4;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.concurrent.*;
 
 public class Test {
-    public static void main(String[] args) {
-        /*Long a = 192168113115L & 2552552400L;
-        Long b = 192168113200L & 2552552400L;;
-        System.out.println(a);
-        System.out.println(b);*/
-
-        /*String s = "2023-01-31";
-        String regex = "^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$|无固定到期日";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(s);
-        if (matcher.matches()) {
-            System.out.println("1");
-        } else {
-            System.out.println("0");
-        }*/
-
-        /**/
-        int a = -8;
-        System.out.println(~a);//7
-        int b = 8;
-        System.out.println(~b);//7
-
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InstantiationException, InvocationTargetException {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Singleton4 instance = Singleton4.getINSTANCE();
+                    System.out.println("通过getINSTANCE得到了对象："+ instance);
+                    Thread.sleep(10000);
+                    System.out.println("线程1正在运行:"+Thread.currentThread().getName());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        System.out.println("主线程1正在运行:"+ Thread.currentThread().getName());
+        Class<Singleton4> singleton4Class = Singleton4.class;
+        Constructor<Singleton4> constructor = singleton4Class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Singleton4 singleton4 = constructor.newInstance();
+        System.out.println("通过反射得到了对象:"+singleton4);
     }
 
 }
